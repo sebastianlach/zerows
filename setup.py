cc567674ddd41675d42d2fdd71b7c8afa0051cef
Sebastian Łach <root@slach.eu>
2015-08-11 14:41:08 +0200

    Migrate to python setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+#-*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='zerows',
+    version='1.0.0',
+    description='Websockets built on top of tornado & zeromq',
+    long_description=long_description,
+    url='https://github.com/sebastianlach/zerows',
+    author='Sebastian Łach',
+    author_email='root@slach.eu',
+    license='MIT',
+    keywords='zerows zero ws zeromq tornado websocket',
+    packages=['zerows'],
+    install_requires=['tornado'],
+    entry_points={
+        'console_scripts': [
+            'zerows=zerows:main',
+        ],
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Topic :: System :: Networking',
+        'Operating System :: Unix',
+    ],
+)
---
+++ zerows/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+usage: zerows [-h] [--xsub-port [XSUB_PORT]] [--xpub-port [XPUB_PORT]]
+                 [--debug]
+                 [host [host ...]]
+"""
+__author__ = "Sebastian Łach"
+__copyright__ = "Copyright 2014, Sebastian Łach"
+__credits__ = ["Sebastian Łach", ]
+__license__ = "MIT"
+__version__ = "1.0"
+__maintainer__ = "Sebastian Łach"
+__email__ = "root@slach.eu"
+__status__ = "Production"
+
+
+def main():
+    """Main entry-point"""
+
+if __name__ == '__main__':
+    main()
+
